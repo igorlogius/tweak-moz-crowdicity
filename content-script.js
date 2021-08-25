052e36eac9f0dc69ad6950be8fc486717efc4aa2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-25 17:11:01 +0200

    fix: next and previous idea button

--- content-script.js
+++ content-script.js
@@ -9,22 +9,27 @@ function addPermaLinks() {
 	const postActions = document.querySelectorAll('div[id^=comment-] p.b-single-idea__comment-actions')
 	for(const postAction of postActions) {
 		// create anchor
-		let a = document.createElement("a")
-		a.innerHTML = '&#128279; permalink';
-		const url = new URL(window.location.href);
-		const origin = url.origin;
-		const path = url.pathname;
-		// get post Id
-		const post = postAction.closest('div[id^="comment-"]');
-		const postid = post.id.split("-")[1];
-		if(postid){
-			a.href = origin + path + "?highlightPost=" + postid;
-			postAction.appendChild(a);
+		if(!postAction.querySelector('a[class="permalink"]')) {
+			let a = document.createElement("a")
+			a.innerHTML = '&#128279; permalink';
+			a.classList.add('permalink');
+			const url = new URL(window.location.href);
+			const origin = url.origin;
+			const path = url.pathname;
+			// get post Id
+			const post = postAction.closest('div[id^="comment-"]');
+			const postid = post.id.split("-")[1];
+			if(postid){
+				a.href = origin + path + "?highlightPost=" + postid;
+				postAction.appendChild(a);
+			}
 		}
 	}
 }
 
+function tweak() {
+	addPermaLinks();
+	expandComments();
+}
 
-addPermaLinks();
-expandComments();
-
+(new MutationObserver(tweak)).observe(document.body, { attributes: true, childList: true, subtree: false});
